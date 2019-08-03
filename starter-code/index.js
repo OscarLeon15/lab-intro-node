a7068fdb1c0760a43639a5a3bb7b5a3f55b88e12
oscar leon <oscar.leon.delmar@gmail.com>
2019-08-03 14:10:30 -0500

    completed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,49 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor(items, length) {
+      this.items = [];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort((a, b) => a-b);
+      this.length ++;
+    }
+    get(pos) {
+      if(this.length || pos > this.length){
+        throw Error("OutOfBounds");
+      }
+      return this.items(pos);
+    }
+    max() {
+      if(!this.length){
+        throw Error ("EmptySortedList");
+      }
+      return this.length(this.length -1);
+      //return this.items[this.length-1]
+    }
+    min() {
+      if(!this.length){
+        throw Error ("EmptySortedList");
+      }
+      return this.item(0);
+    }
+    average() {
+      if(!this.length){
+        throw Error("EmptySortedList")
+      }
+      return this.sum()/this.length;
+    }
+    sum() {
+      if(!this.length){
+        return 0;
+      }
+      return this.items.reduce((total, currentValue)=> total+ currentValue);
+    }
   };
+
+  // class List extends SortedList{
+  //   constructor(){
+  //   }
+  // }
   
   module.exports = SortedList;
